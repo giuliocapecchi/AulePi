908d578ef14f516e36d14d744a82f232420fd4d1
giuliocapecchi <giuliocapecchi2000@gmail.com>
2024-10-26 19:15:36 +0200

    changed backend url to the real one

--- frontend/app/api/open-classrooms/route.ts
+++ frontend/app/api/open-classrooms/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 interface dataFormat {
     building: string;  // Il nome del polo
-    building_code: string;  // Ancora nome del polo //TODO : Da rimuovere
+    building_code: string;  // Ancora nome del polo
     rooms: {
         roomNumber: string;  // Nome o numero dell'aula (location)
         slots: { 
@@ -18,37 +18,6 @@ interface dataFormat {
     distance: number;  // Distanza dalla posizione attuale, se calcolata
 }
 
-// GENERAL STRUCTURE OF THE DATA
-// {
-//     "poloA": {
-//         "IngA11": {
-//             "free": false,
-//             "lessons": [
-//                 {
-//                     "end": "2024-10-24 18:30:00",
-//                     "professor": "CARMIGNANI GIONATA\\, MARRAZZINI LEONARDO",
-//                     "start": "2024-10-24 16:30:00",
-//                     "summary": "SISTEMI PRODUTTIVI"
-//                 }
-//             ]
-//         },
-//         "IngA12": {
-//             "free": true,
-//             "lessons": []
-//         },
-//         "IngA13": {
-//             "free": false,
-//             "lessons": [
-//                 {
-//                     "end": "2024-10-24 19:30:00",
-//                     "professor": "No description",
-//                     "start": "2024-10-24 17:30:00",
-//                     "summary": "SANTO FEDERICA - Tutor Ing. Civile"
-//                 }
-//             ]
-//         }
-//     }
-// }
 
 
 export async function POST(req: Request) {
@@ -58,7 +27,7 @@ export async function POST(req: Request) {
 
         // Send the user location to the backend
         const response = await fetch(
-            "http://127.0.0.1:8080/api/open-classrooms", // TODO : Change this to the backend URL
+            process.env.BACKEND_URL+"/api/open-classrooms", // TODO : Change this to the backend URL
             {
                 method: "POST",
                 headers: {
@@ -94,7 +63,7 @@ export async function GET() {
     try {
         // Fetch the default data without location
         const response = await fetch(
-            "http://127.0.0.1:8080/api/open-classrooms",
+            process.env.BACKEND_URL+"/api/open-classrooms",
             {
                 method: "GET",
                 cache: "no-cache",
