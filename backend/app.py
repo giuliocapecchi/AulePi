0b03f9554b5771d2e1952a6fce2c3213502efe31
giuliocapecchi <giuliocapecchi2000@gmail.com>
2024-10-26 18:21:23 +0200

    -

--- backend/app.py
+++ backend/app.py
@@ -82,4 +82,5 @@ def get_open_classrooms():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(port=8080)
