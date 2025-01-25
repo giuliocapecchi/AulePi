f2bb452a78a6e2f777e611c8634b7dd1c1684765
giuliocapecchi <giuliocapecchi2000@gmail.com>
2025-01-25 12:27:29 +0100

    updated loading spinner

--- frontend/components/Loading.tsx
+++ frontend/components/Loading.tsx
@@ -6,13 +6,10 @@ interface LoadingProps {
     fadeOut: boolean;
 }
 
-import Image from 'next/image';
-import logo from '/public/logo.svg';
 
 const Loading: React.FC<LoadingProps> = ({ fadeOut }) => {
     return (
         <div className={`loading ${fadeOut ? 'fade-out' : ''}`} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', color: '#fff', backgroundColor: 'black' }}>
-            <Image src={logo} alt="Logo" style={{ marginBottom: '10px' }} />
             <div className="loader"></div>
         </div>
     );
